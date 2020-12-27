547a84e5a6a95140167ca6d25808319828d1716a
JYHsu <hsujoyao@gmail.com>
2020-12-27 20:13:27 +0800

    git start & simplefactory

---
+++ SimpleFactory/Operation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    class Operation
+    {
+        private double _numberA = 0;
+        private double _numberB = 0;
+
+        public double numberA
+        {
+            get { return _numberA; }
+            set { _numberA = value; }
+        }
+        public double numberB
+        {
+            get { return _numberB; }
+            set { _numberB = value; }
+        }
+
+        public virtual double GetResult()
+        {
+            double result = 0;
+            return result;
+        }
+    }
+
+    class OperationAdd: Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = numberA + numberB;
+            return result;
+        }
+    }
+
+    class OperationSub : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = numberA - numberB;
+            return result;
+        }
+    }
+
+    class OperationMul : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = numberA * numberB;
+            return result;
+        }
+    }
+
+    class OperationDiv : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            if (numberB == 0)
+            {
+                throw new Exception("除數不能為0。");
+            }
+            result = numberA / numberB;
+            return result;
+        }
+    }
+}
---
+++ SimpleFactory/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation oper;
+            oper = OperationFactory.createOperation("+");
+            oper.numberA = 1;
+            oper.numberB = 5;
+            double result = oper.GetResult();
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
