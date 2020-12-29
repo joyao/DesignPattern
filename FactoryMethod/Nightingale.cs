3c0fd5819aca62cd256a987ff61e9760ecffe471
JYHsu <hsujoyao@gmail.com>
2020-12-29 17:06:29 +0800

    Add temp FactoryMethod Pattern

---
+++ FactoryMethod/Nightingale.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Nightingale
+    {
+        public void Sweep()
+        {
+            Console.WriteLine("sweep");
+        }
+        
+        public void Wash()
+        {
+            Console.WriteLine("wash");
+        }
+
+        public void BuyRice()
+        {
+            Console.WriteLine("buy rice");
+        }
+    }
+
+    class Undergraduate : Nightingale
+    {
+
+    }
+    class Volunteer : Nightingale
+    {
+
+    }
+}
---
+++ FactoryMethod/SimpleFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class SimpleFactory
+    {
+        public static Nightingale CreateNightingale(string type)
+        {
+            Nightingale result = null;
+            switch (type)
+            {
+                case "undergraduate":
+                    result = new Undergraduate();
+                    break;
+                case "volunteer":
+                    result = new Volunteer();
+                    break;
+            }
+            return result;
+        }
+    }
+}
