9008400beecc6699a9d4ecaf3ee34e3b6e786cfd
JYHsu <hsujoyao@gmail.com>
2020-12-29 12:40:41 +0800

    Add Proxy Pattern

---
+++ Proxy/GiveGift.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    interface IGiveGift
+    {
+        void GiveDolls();
+        void GiveFlowers();
+        void GiveChocolatte();
+    }
+
+    class Pursuit: IGiveGift
+    {
+        SchoolGirl mm;
+        public Pursuit(SchoolGirl mm)
+        {
+            this.mm = mm;
+        }
+        public void GiveDolls()
+        {
+            Console.WriteLine(mm.Name + ", these dolls are for u.");
+        }
+        public void GiveFlowers()
+        {
+            Console.WriteLine(mm.Name + ", these flowers are for u.");
+        }
+        public void GiveChocolatte()
+        {
+            Console.WriteLine(mm.Name + ", these chocolatte are for u.");
+        }
+    }
+
+    class Proxy: IGiveGift
+    {
+        Pursuit gg;
+        public Proxy(SchoolGirl mm)
+        {
+            gg = new Pursuit(mm);
+        }
+        public void GiveDolls()
+        {
+            gg.GiveDolls();
+        }
+        public void GiveFlowers()
+        {
+            gg.GiveFlowers();
+        }
+        public void GiveChocolatte()
+        {
+            gg.GiveChocolatte();
+        }
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SchoolGirl yp = new SchoolGirl();
+            yp.Name = "YP";
+
+            Proxy JJ = new Proxy(yp);
+
+            JJ.GiveDolls();
+            JJ.GiveFlowers();
+            JJ.GiveChocolatte();
+
+            Console.Read();
+        }
+    }
+}
