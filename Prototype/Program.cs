9cf238f73d8fdb0566aac8868fb75c43645fc8c5
JYHsu <hsujoyao@gmail.com>
2020-12-30 15:41:48 +0800

    Add Prototype Pattern

---
+++ Prototype/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resume a = new Resume("JY");
+            a.SetPersonalInfo("Female", "24");
+            a.SetWorkExperience("2012-2016", "NCCU");
+
+            Resume b = (Resume)a.Clone();
+            b.SetWorkExperience("2016-2018", "NTU");
+
+            Resume c = (Resume)a.Clone();
+            c.SetPersonalInfo("Female", "26");
+            c.SetWorkExperience("2016-2018", "NTU");
+
+            a.Display();
+            b.Display();
+            c.Display();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Prototype/Resume.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    // 履歷
+    class Resume : ICloneable
+    {
+        private string name;
+        private string sex;
+        private string age;
+        private WorkExperience work;
+
+        public Resume(string name)
+        {
+            this.name = name;
+            work = new WorkExperience();
+        }
+        private Resume(WorkExperience work)
+        {
+            this.work = (WorkExperience)work.Clone();
+        }
+
+        // 設定個人資訊
+        public void SetPersonalInfo(string sex, string age)
+        {
+            this.sex = sex;
+            this.age = age;
+        }
+
+        // 設定工作經歷
+        public void SetWorkExperience(string workDate, string company)
+        {
+            work.WorkDate = workDate;
+            work.Company = company;
+        }
+
+        // 顯示
+        public void Display()
+        {
+            Console.WriteLine("{0} {1} {2}", name, sex, age);
+            Console.WriteLine("工作經歷：{0} {1}", work.WorkDate, work.Company);
+        }
+
+        public Object Clone()
+        {
+            Resume obj = new Resume(this.work);
+            obj.name = this.name;
+            obj.sex = this.sex;
+            obj.age = this.age;
+            return obj;
+        }
+    }
+}
