553889dbb384acd83843c60ca42115fce4f2b745
JYHsu <hsujoyao@gmail.com>
2020-12-31 15:14:41 +0800

    Add Builder Pattern

---
+++ Builder/Builder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    abstract class Builder
+    {
+        public abstract void BuildPartA();
+        public abstract void BuildPartB();
+        public abstract Product GetResult();
+    }
+
+    class ConcreteBuilder1 : Builder
+    {
+        private Product product = new Product();
+
+        public override void BuildPartA()
+        {
+            product.Add("零件A");
+        }
+
+        public override void BuildPartB()
+        {
+            product.Add("零件B");
+        }
+
+        public override Product GetResult()
+        {
+            return product;
+        }
+    }
+    class ConcreteBuilder2 : Builder
+    {
+        private Product product = new Product();
+
+        public override void BuildPartA()
+        {
+            product.Add("零件X");
+        }
+
+        public override void BuildPartB()
+        {
+            product.Add("零件Y");
+        }
+
+        public override Product GetResult()
+        {
+            return product;
+        }
+    }
+}
---
+++ Builder/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Product
+    {
+        IList<string> parts = new List<string>();
+
+        public void Add(string part)
+        {
+            parts.Add(part);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("\n產品 建立 ------");
+            foreach (string part in parts)
+            {
+                Console.WriteLine(part);
+            }
+        }
+    }
+}
