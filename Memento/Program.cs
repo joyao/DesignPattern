3e13e09d7f6d288250bbd120da8d1e7ad58504ac
JYHsu <hsujoyao@gmail.com>
2021-01-01 16:58:42 +0800

    Add Memento Pattern

---
+++ Memento/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 大戰Boss前
+            GameRole jy = new GameRole();
+            jy.GetInitState();
+            jy.StateDisplay();
+
+            // 保存進度
+            RoleStateCaretaker stateAdmin = new RoleStateCaretaker();
+            stateAdmin.Memento = jy.SaveState();
+
+            // 大戰Boss時，損耗嚴重
+            jy.Fight();
+            jy.StateDisplay();
+
+            // 恢復之前狀態
+            jy.RecoveryState(stateAdmin.Memento);
+            jy.StateDisplay();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Memento/RoleStateCaretaker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class RoleStateCaretaker
+    {
+        private RoleStateMemento memento;
+        public RoleStateMemento Memento
+        {
+            get { return memento; }
+            set { memento = value; }
+        }
+    }
+}
