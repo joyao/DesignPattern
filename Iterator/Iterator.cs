db2e91f38dbfef61bcd9048af12909b0374e8b1d
JYHsu <hsujoyao@gmail.com>
2021-01-02 03:20:58 +0800

    Add Iterator Pattern

---
+++ Iterator/Iterator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    abstract class Iterator
+    {
+        public abstract object First();
+        public abstract object Next();
+        public abstract bool IsDone();
+        public abstract object CurrentItem();
+    }
+
+    class ConcreteIterator : Iterator
+    {
+        private ConcreteAggregate aggregate;
+        private int current = 0;
+
+        public ConcreteIterator(ConcreteAggregate aggregate)
+        {
+            this.aggregate = aggregate;
+        }
+
+        public override object First()
+        {
+            return aggregate[0];
+        }
+
+        public override object Next()
+        {
+            object ret = null;
+            current++;
+            if (current < aggregate.Count)
+            {
+                ret = aggregate[current];
+            }
+            return ret;
+        }
+
+        public override bool IsDone()
+        {
+            return current >= aggregate.Count ? true : false;
+        }
+
+        public override object CurrentItem()
+        {
+            return aggregate[current];
+        }
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteAggregate a = new ConcreteAggregate();
+
+            a[0] = "p1";
+            a[1] = "p2";
+            a[2] = "p3";
+            a[3] = "p4";
+            a[4] = "p5";
+            a[5] = "p6";
+
+            Iterator i = new ConcreteIterator(a);
+            object item = i.First();
+            while (!i.IsDone())
+            {
+                Console.WriteLine("{0} 請買車票!", i.CurrentItem());
+                i.Next();
+            }
+
+            Console.Read();
+
+            //IList<string> a = new List<string>();
+            //a.Add("p1");
+            //a.Add("p2");
+            //a.Add("p3");
+            //a.Add("p4");
+            //a.Add("p5");
+            //a.Add("p6");
+
+            //foreach (string item in a)
+            //{
+            //    Console.WriteLine("{0} 請買車票!", item);
+            //}
+
+            //Console.Read();
+        }
+    }
+}
