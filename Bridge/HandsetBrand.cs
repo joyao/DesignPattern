26ddf8165d9d3a433aba2b8ed8539ac2341e1416
JYHsu <hsujoyao@gmail.com>
2021-01-02 22:44:23 +0800

    Add Bridge Pattern

---
+++ Bridge/HandsetBrand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    abstract class HandsetBrand
+    {
+        protected HandsetSoft soft;
+
+        // 設定手機軟體
+        public void SetHandsetSoft(HandsetSoft soft)
+        {
+            this.soft = soft;
+        }
+        // 執行
+        public abstract void Run();
+    }
+
+    // 手機品牌N
+    class HandsetBrandN : HandsetBrand
+    {
+        public override void Run()
+        {
+            soft.Run();
+        }
+    }
+
+    // 手機品牌M
+    class HandsetBrandM : HandsetBrand
+    {
+        public override void Run()
+        {
+            soft.Run();
+        }
+    }
+}
---
+++ Bridge/HandsetSoft.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    abstract class HandsetSoft
+    {
+        public abstract void Run();
+    }
+
+    // 手機遊戲
+    class HandsetGame : HandsetSoft
+    {
+        public override void Run()
+        {
+            Console.WriteLine("執行手機遊戲");
+        }
+    }
+
+    // 手機通訊錄
+    class HandsetAddressList : HandsetSoft
+    {
+        public override void Run()
+        {
+            Console.WriteLine("執行手機通訊錄");
+        }
+    }
+}
