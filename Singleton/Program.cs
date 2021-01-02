0b785fc49a4f99687a0d4a1a5a8488e2dc82a027
JYHsu <hsujoyao@gmail.com>
2021-01-02 18:33:04 +0800

    Add Singleton Pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("兩個物件是相同的實體");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Singleton
+    {
+        private static Singleton instance;
+        private static readonly object syncRoot = new object();
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                }
+            }
+
+            return instance;
+        }
+    }
+}
